acb8f8565e3b0fe6ef1d3a743767bbfac6f026ec
Gurami Kurashvili <vasatoto@gmail.com>
2019-02-22 20:12:12 +0400

    add

---
+++ button/App2/Buttons.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace App2
+{
+    [Activity(Label = "Buttons")]
+    class Buttons : Activity
+    {
+
+        MainActivity Number = new MainActivity();
+        
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.Buttons);
+
+
+            LinearLayout ll = FindViewById<LinearLayout>(Resource.Id.ll222);
+
+            
+
+            for (int i = 1; i<= MainActivity.number; i++)
+            {
+                var button1 = new Button(this);
+                button1.Text = i.ToString();
+                ll.AddView(button1);
+                button1.Click += delegate
+                {
+                    
+                    Toast.MakeText(this, button1.Text, ToastLength.Short).Show();
+                };
+            }
+
+        }
+    }
+}
---
+++ button/App2/MainActivity.cs
@@ -0,0 +1,48 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using System;
+using Android.Content;
+
+namespace App2
+{
+    [Activity(Label = "Buttons", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        public static int number;
+        Button button1;
+        EditText EditText1;
+        
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            button1 = FindViewById<Button>(Resource.Id.submit);
+            EditText1 = FindViewById<EditText>(Resource.Id.GetText);
+
+
+            button1.Click += submit_click;
+
+            
+        }
+        void submit_click(object sernder, EventArgs e)
+        {
+
+
+            Intent intent = new Intent(this, typeof(Buttons));
+
+            StartActivity(intent);
+
+            number = Convert.ToInt32(EditText1.Text);
+
+
+
+
+
+        }
+    }
+}
